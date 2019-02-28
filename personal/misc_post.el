96934df474da8fa6d8c37439a469aed36050bec4
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-02-28 10:39:38 +0100

    Addded hook for google style to also work in c-mode.

--- personal/misc_post.el
+++ personal/misc_post.el
@@ -41,5 +41,7 @@
 ;; Google c style
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(add-hook 'c-mode-hook 'google-set-c-style)
+(add-hook 'c-mode-hook 'google-make-newline-indent)
 (add-hook 'c++-mode-hook 'google-set-c-style)
 (add-hook 'c++-mode-hook 'google-make-newline-indent)
