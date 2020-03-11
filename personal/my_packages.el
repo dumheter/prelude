7b3202ccd56495a076249cdaff3b0cb7b1b7e58a
Christoffer Gustafsson <christoffer.golf@gmail.com>
2020-03-11 14:39:28 +0100

    fixed json setting.

--- personal/my_packages.el
+++ personal/my_packages.el
@@ -50,7 +50,7 @@
 
 ;; https://www.reddit.com/r/emacs/comments/audffp/tip_how_to_use_a_stable_and_fast_environment_to/
 (use-package lsp-mode
-             :hook (prog-mode . lsp))
+           :hook (prog-mode . lsp))
 
 (use-package ccls
              :after projectile
