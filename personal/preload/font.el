419f1bf205be8b8d24c481fccabcacf1ade5d026
Christoffer Gustafsson <christoffer.golf@gmail.com>
2020-09-05 17:20:30 +0200

    Using the new font on windows. Added a desc on how to run emacs clients as daemon on windows.

--- personal/preload/font.el
+++ personal/preload/font.el
@@ -1,8 +1,12 @@
 ;; If on windows, set font
+;; Get the font from https://github.com/microsoft/cascadia-code/releases
 (cond
  ((string-equal system-type "windows-nt")
   (progn
-    (set-frame-font "Consolas"))))
+    (add-to-list 'default-frame-alist '(font . "Cascadia Mono PL"))
+    (set-face-attribute 'default t
+		        :font "Cascadia Mono PL"
+		        :height 90))))
 
 (cond
  ((string-equal system-type "gnu/linux")
---
+++ personal/windows/AutorunDaemon.md
@@ -0,0 +1,4 @@
+# Auto Start Emacs Daemon for Windows
+1. Open explorer and type `shell:startup`.
+2. Now create a shortcut and write `"C:\path\to\runemacs.exe" --daemon`
+3. Profit.
